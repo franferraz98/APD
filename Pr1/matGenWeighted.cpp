9c65ee08129428b550e16497a169ff981a9fb2ab
Fran Ferraz <franferrazg@gmail.com>
2020-01-04 18:41:27 +0100

    Add weighted matrix generator

---
+++ Pr1/matGenWeighted.cpp
@@ -0,0 +1,69 @@
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include <chrono>
+
+#define N 3
+
+using namespace std;
+
+int** create2DArray(unsigned height, unsigned width)
+{
+    random_device dev;
+    mt19937 gen(dev());
+    uniform_int_distribution<int> distrib(0,10);
+    int** array2D = 0;
+    array2D = new int*[height];
+
+    for (int h = 0; h < height; h++) {
+        array2D[h] = new int[width];
+    }
+
+    for (int h = 0; h < height; h++) {
+
+        for (int w = h; w < width; w++) {
+            int rand = distrib(gen);
+            array2D[h][w] = rand;
+            array2D[w][h] = rand;
+        }
+    }
+
+    return array2D;
+}
+
+int main(){
+
+    auto t1 = chrono::high_resolution_clock::now();
+
+    int** my2DArray = create2DArray(N, N);
+
+    for (int h = 0; h < N; h++)
+    {
+        for (int w = 0; w < N; w++)
+        {
+                printf("%i,", my2DArray[h][w]);
+        }
+        printf("\n");
+    }
+
+    // important: clean up memory
+    printf("\n");
+    printf("Cleaning up memory...\n");
+    for (int  h = 0; h < N; h++)
+    {
+    delete [] my2DArray[h];
+    }
+    delete [] my2DArray;
+    my2DArray = 0;
+    printf("Ready.\n");
+
+    auto t2 = chrono::high_resolution_clock::now();
+
+    auto time = chrono::duration_cast<chrono::milliseconds>(t2-t1).count();
+
+    cout << "Time elapsed: " << time << "ms" << endl;
+
+    return 0;
+}
